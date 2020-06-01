4c4da4545e94cfca69df598087faec15c19c54a0
dmitriy.z <dmitriy.z@siteplus.com>
2020-06-01 23:59:01 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1221,9 +1221,30 @@ const createDataTable = async (id, config, isFixedCollumns) => {
 
 	var table = $(id).DataTable({ ...DEFAULT_DATA_TABLE_CONFIG, ...config });
 
-	$('thead th, .DTFC_LeftHeadWrapper th:not(.nosort), .DTFC_RightHeadWrapper th:not(.nosort)').append(
-		'<svg class="sort-arrow" xmlns="http://www.w3.org/2000/svg" width="4.211" height="10" viewBox="0 0 4.211 10"><path d="M13,10.105,10.895,8V9.579H3v1.053h7.895v1.579Z" transform="translate(12.211 -3) rotate(90)" fill="#212b35"/></svg>'
-	);
+	$(table.table().container()).find('tfoot').each(function() {
+		$(this).find('th').each(function(i) {
+			$(this).find('input').attr('data-index', i);
+		});
+	});
+
+	var canSearch = false;
+
+	function filterTable(element, value) {
+		if (table.settings()[0].jqXHR) table.settings()[0].jqXHR.abort();
+		table.column($(element).data('index')).search(value).draw();
+	}
+
+	$(table.table().container()).on('keyup', 'tfoot input', function() {
+		var input = this;
+
+		if ($(this).val().length > 2) {
+			canSearch = true;
+			filterTable(input, input.value);
+		} else if (canSearch) {
+			filterTable(input, '');
+			canSearch = false;
+		}
+	});
 
 	if ($('.diriq-table__wrapper._with-endpoint').length > 0) {
 		$('.diriq-table__wrapper._with-endpoint').each(function() {
@@ -1232,12 +1253,20 @@ const createDataTable = async (id, config, isFixedCollumns) => {
 		});
 	}
 
+	$('thead th, .DTFC_LeftHeadWrapper th:not(.nosort), .DTFC_RightHeadWrapper th:not(.nosort)').append(
+		'<svg class="sort-arrow" xmlns="http://www.w3.org/2000/svg" width="4.211" height="10" viewBox="0 0 4.211 10"><path d="M13,10.105,10.895,8V9.579H3v1.053h7.895v1.579Z" transform="translate(12.211 -3) rotate(90)" fill="#212b35"/></svg>'
+	);
+
 	$('.datatable._contact-details-lists input:checkbox').change(function() {
 		if ($(this).is(':checked')) {
 			$(this).closest('tr').addClass('_active');
 		} else {
 			$(this).closest('tr').removeClass('_active');
 		}
+	});
+
+	$('.diriq-table__filter-btn').click(function() {
+		$('body').toggleClass('_filter-visible');
 	});
 };
 
